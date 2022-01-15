8a883455b64f03c052024d724327a7765b7c704a
Nishant Singh <nissingh@linkedin.com>
2022-01-15 14:42:53 +0530

    Bit shift operator

--- freeCodeCamp/11_numeric_bitshift.go
+++ freeCodeCamp/11_numeric_bitshift.go
@@ -1,9 +1,11 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
-func main(){
-
+func main() {
+	a := 8              // 1000 in binary
+	fmt.Println(a << 3) // Left shift a by 3 places
+	fmt.Println(a >> 3) // Right shift a by 3 places
 }
